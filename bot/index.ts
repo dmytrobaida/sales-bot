0007727876b10b1e946df853d63a915e04508687
dmytrobaida <dmytrobaida@outlook.com>
2022-08-18 23:32:03 +0300

    fixes issue with db

--- bot/index.ts
+++ bot/index.ts
@@ -11,12 +11,10 @@ const adminChatId = process.env.ADMIN_CHAT_ID;
 const faunaDbsecret = process.env.FAUNA_DB_SECRET;
 
 const bot = new Telegraf(token);
-const client = new Client({
-    secret: faunaDbsecret,
-    domain: 'db.fauna.com',
-    port: 443,
-    scheme: 'https',
-});
+// const client = new Client({
+//     secret: faunaDbsecret,
+//     domain: "db.us.fauna.com",
+// });
 
 enum CTCommand {
     getSales = 'getsales',
@@ -45,11 +43,11 @@ export default async function getBot(setHook: boolean = false, hookUrl?: string)
         console.log(ctx.message);
         await ctx.telegram.sendMessage(adminChatId, `Користувач ID:${ctx.chat.id} хоче приєднатися до боту!`);
 
-        await client.query(query.Create(query.Collection('news_receivers', {
-            data: {
-                chatId: ctx.chat.id,
-            }
-        })))
+        // await client.query(query.Create(query.Collection('news_receivers', {
+        //     data: {
+        //         chatId: ctx.chat.id,
+        //     }
+        // })))
     });
 
     bot.command(CTCommand.getSales, async (ctx) => {
