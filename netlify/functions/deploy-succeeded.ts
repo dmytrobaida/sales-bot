03a017304803d1aa1146caae5933acc682b4e05d
dmytrobaida <dmytrobaida@outlook.com>
2022-08-19 17:43:22 +0300

    updates func

--- netlify/functions/deploy-succeeded.ts
+++ netlify/functions/deploy-succeeded.ts
@@ -2,7 +2,7 @@ import { Handler } from "@netlify/functions";
 
 export const handler: Handler = async (event, context) => {
     console.log(event);
-    console.log(context);
+    // console.log(context);
     return {
         statusCode: 200,
     };
