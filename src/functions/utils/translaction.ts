7a6858eb0868c1d33af93c9ef77b3f68b7e536d8
dmytrobaida <dmytrobaida@outlook.com>
2022-08-21 22:08:26 +0300

    fix 2

--- src/functions/utils/translaction.ts
+++ src/functions/utils/translaction.ts
@@ -1,41 +1,34 @@
-import i18next, { Resource, ResourceLanguage } from "i18next";
+import i18next, { i18n, Resource, ResourceLanguage } from "i18next";
 import { Context } from "telegraf";
 
 import en from 'assets/translations/en.json';
-import ua from 'assets/translations/ua.json';
+import uk from 'assets/translations/uk.json';
 
-type GetContextFn = () => Context;
-type Lang = 'en' | 'ua';
+type Lang = 'en' | 'uk';
 
 const DefaultLang: Lang = 'en';
 const Languages: Record<Lang, ResourceLanguage> = {
     en: en,
-    ua: ua,
+    uk: uk,
 };
 
-export async function getI18Next(getContext: GetContextFn) {
-    await i18next
-        .use({
-            type: 'languageDetector',
-            init: () => { },
-            cacheUserLanguage: () => { },
-            detect: () => {
-                const context = getContext();
-                const languageCode = context.from.language_code || DefaultLang;
-                console.log(`Detected chat language: ${languageCode}`);
-                return languageCode;
-            }
-        })
-        .init({
-            debug: true,
-            fallbackLng: DefaultLang,
-            resources: Object.keys(Languages).reduce((resources, lang: Lang) => {
-                resources[lang] = {
-                    translation: Languages[lang],
-                };
-                return resources;
-            }, {} as Resource),
-        });
+let i18nInstance: i18n = null;
 
-    return i18next;
+export async function getI18Next() {
+    if (i18nInstance == null) {
+        await i18next
+            .init({
+                debug: true,
+                fallbackLng: DefaultLang,
+                resources: Object.keys(Languages).reduce((resources, lang: Lang) => {
+                    resources[lang] = {
+                        translation: Languages[lang],
+                    };
+                    return resources;
+                }, {} as Resource),
+            });
+        i18nInstance = i18next;
+    }
+
+    return i18nInstance;
 }
